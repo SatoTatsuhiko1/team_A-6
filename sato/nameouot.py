de63afaf4eba822cdbb6c7d4ac5679fdfc65c802
SatoTatsuhiko1 <sato.tatsuhiko.tc@mynavi.jp>
2024-05-14 07:55:34 +0000

    演習1日目

--- sato/nameouot.py
+++ sato/nameouot.py
@@ -1,9 +1,4 @@
 import sys
 args = sys.argv
-
-input1 = args[1]
-input2 = args[2]
-input3 = args[3]
-
-sum = input1 + input2 + input3 
-print(sum)
+name = args[1]
+print("Hello " + name + " !", end="")
--- sato/oddeven.py
+++ sato/oddeven.py
@@ -2,5 +2,8 @@ import sys
 args = sys.argv
 num = args[1]
 num=int(num)
+#偶数か奇数か判定
 if num % 2 == 0:
-    print("偶数です")
+    print("偶数",end="")
+else:
+    print("奇数",end="")
