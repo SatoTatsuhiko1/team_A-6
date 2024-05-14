241394849f26621772d440e4564f21b66a41ba37
SatoTatsuhiko1 <sato.tatsuhiko.tc@mynavi.jp>
2024-05-14 08:20:27 +0000

    演習5改

--- sato/test.py
+++ sato/test.py
@@ -8,7 +8,7 @@ eng = args[3]
 math = int(math)
 jap = int(jap)
 eng = int(eng)
-
+#3教科合計
 sum=math+jap+eng
 #合否判定
 is_check =(((math>=70)and(jap>=70)and(eng>=70))or(sum>=220))and((math>=50)and(jap>=50)and(eng>=50))
